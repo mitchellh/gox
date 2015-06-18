de8271a6e0f4872e9238a56dca6ea7b2dbb6b3b9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-06-18 16:08:24 +0200

    typo

--- main_osarch.go
+++ main_osarch.go
@@ -8,7 +8,7 @@ func mainListOSArch(version string) int {
 	fmt.Printf(
 		"Supported OS/Arch combinations for %s are shown below. The \"default\"\n"+
 			"boolean means that if you don't specify an OS/Arch, it will be\n"+
-			"included by default. If it isn't a deafult OS/Arch, you must explicitly\n"+
+			"included by default. If it isn't a default OS/Arch, you must explicitly\n"+
 			"specify that OS/Arch combo for Gox to use it.\n\n",
 		version)
 	for _, p := range SupportedPlatforms(version) {
