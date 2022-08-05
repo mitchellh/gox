b2ace3c1947cbe963f38cf49afb57f456c3da15e
WANG Xuerui <git@xen0n.name>
2022-08-05 12:31:21 +0800

    Add go1.19
    Fixes #165.

--- platform.go
+++ platform.go
@@ -178,7 +178,11 @@ var (
 	// no new platforms in 1.18
 	Platforms_1_18 = Platforms_1_17
 
-	PlatformsLatest = Platforms_1_18
+	Platforms_1_19 = addDrop(Platforms_1_18, []Platform{
+		{"linux", "loong64", true},
+	}, nil)
+
+	PlatformsLatest = Platforms_1_19
 )
 
 // SupportedPlatforms returns the full list of supported platforms for
@@ -221,6 +225,7 @@ func SupportedPlatforms(v string) []Platform {
 		{">= 1.16, < 1.17", Platforms_1_16},
 		{">= 1.17, < 1.18", Platforms_1_17},
 		{">= 1.18, < 1.19", Platforms_1_18},
+		{">= 1.19, < 1.20", Platforms_1_19},
 	}
 
 	for _, p := range platforms {
